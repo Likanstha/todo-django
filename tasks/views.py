16f7691c6b9971ce5a1f24cf37ae290ea1c19341
likan stha <likan.stha@gmail.com>
2025-03-31 09:33:33 +1100

    task form add

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
 from .models import *
@@ -9,5 +9,12 @@ def index(request):
 
 
     form = TaskForm()
+
+    if request.method =='POST' :
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
     context = {'tasks' : tasks, 'form' : form}
     return render(request, 'tasks/list.html',context)
