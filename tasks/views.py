eb0ca274f970df769838fd5071f5ff3c82068b2e
likan stha <likan.stha@gmail.com>
2025-03-31 10:04:59 +1100

    delete actions

--- tasks/views.py
+++ tasks/views.py
@@ -23,4 +23,20 @@ def updateTask(request, pk):
 
     task = Task.objects.get(id=pk)
 
-    return render(request, 'tasks/update_task.html')
+    form = TaskForm(instance = task)
+    context ={'form': form}
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    return render(request, 'tasks/update_task.html',context)
+
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    context ={'item':item}
+    return render(request,'tasks/delete.html', context)
